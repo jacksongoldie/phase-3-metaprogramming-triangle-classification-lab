4d8e6bf25a5bfd3ad45c31eb733f0eecc4ded277
Goldie <goldiejackson25@yahoo.com>
2021-11-29 05:32:59 -0600

    commit tree 4f725c1a429b58650b2783040cf393abcb69983f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :l1, :l2, :l3
+  
+  def initialize(l1, l2, l3)
+    if l1 == 0 || l2 == 0 || l3 == 0 
+        begin
+          raise TriangleError
+        rescue TriangleError => error 
+          puts error.message
+        else 
+          @l1 = l1
+          @l2 = l2
+          @l3 = l3
+        end
+  end
+
+  def kind
+    
+    if @l1 == @l2 && @l1 == @l3
+      :equilateral
+    elsif @l1 == @l2 && @l2 != @l3
+      :isosceles
+    elsif @l2 == @l3 && @l1 != @l3
+      :isosceles
+    elsif @l3 == @l1 && @l2 != @l3
+      :isosceles
+    elsif @l1 != @l2 && @l2 != @l3
+      :scalene
+  end
 end
+
+  class TriangleError < StandardError
+    def message
+      "you messed up"
+    end
+  end
+
+end
+binding.pry
